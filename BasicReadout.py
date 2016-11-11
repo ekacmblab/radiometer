a1c06e40843b13eb147167e8ebf8910313b370b8
adrianaprotondo <adrianaprotondo@gmail.com>
2016-11-11 11:23:14 -0500

    Added comments to BasicReadout.py and code for writing header with info of data taking

--- BasicReadout.py
+++ BasicReadout.py
@@ -1,24 +1,57 @@
 import Readout as r
 import time
 import numpy as np
-import pylab as pl
 
+"""" """
+#=========
+# Presets
+#=========
+
+# Duration of the read
 duration = 5*60
 
+# Set the path and name for the file where the data will be wirtten
+# the file name will be of the form: path+"%Y-%m-%d_%H:%M:%S"+extension
+# e.g "./Data/2016-11-10_10:20:50_Readout.txt"
+# the extension must be a .txt file
+path = "./Data/"
+extension = "_Readout.txt"
 
-multimeter = r.Readout()
-
-read = multimeter.readLoop(duration)
-
-date = time.strftime("./Data/%Y-%m-%d_%H:%M:%S")
+# Set heading information
+date = time.strftime("%Y-%m-%d_%H:%M:%S")
+# where the horn was pointing
 looking = "East window Pupin 605"
+# angle of the horn
 angle = "80"
+# temperatures
 temperatureOutside = ""
 temperatureCalibrator = ""
 weather = "clear"
 duration = str(duration)
+# was the calibrator in front
 calibrator = "Yes"
 
-timestr = date+"_Readout.txt"
-np.savetxt(timestr,read)
+#===========
+# Read Data
+#===========
+
+# Create a new multimeter of the class Readout to read data
+multimeter = r.Readout()
+
+# Read for the duration set
+read = multimeter.readLoop(duration)
+
+#============
+# Write Data
+#============
+
+title = path + date + extension
+
+# Create header for the file with all the information
+# The header has 8 lines all satrting with #
+header = "{0}\nDuration: {7}\nPointing Position of the Horn: {1}\nAngle pointing: {2}\nCalibrator used: " \
+         "{3}\nTemperature Outside: {4}\nTemperature of the calibrator: {5}\nWeather: {6}".format(
+    title, looking, angle, calibrator, temperatureOutside,temperatureCalibrator,weather,duration)
+
+np.savetxt(title,read,header=header)
 
