aad3390334c1cfe00d87f724040ff9f1a4858a24
adrianaprotondo <adrianaprotondo@gmail.com>
2016-11-11 12:01:16 -0500

    Added header to the python file

--- BasicReadout.py
+++ BasicReadout.py
@@ -1,11 +1,21 @@
-import Readout as r
+# !/usr/bin/env python
+# title           :BasicReadout.py
+# description     :This file reads data fro multimeter and writes to a file in disk
+# author          :Adriana Perez Rotondo
+# date            :2016/11/11
+# version         :0.1
+# usage           :python BasicReadout.py
+# notes           :
+# python_version  :2.7.10
+# ==============================================================================
+
+import Readout
 import time
 import numpy as np
 
-"""" """
-#=========
+# =========
 # Presets
-#=========
+# =========
 
 # Duration of the read
 duration = 5*60
@@ -31,19 +41,19 @@ duration = str(duration)
 # was the calibrator in front
 calibrator = "Yes"
 
-#===========
+# ===========
 # Read Data
-#===========
+# ===========
 
 # Create a new multimeter of the class Readout to read data
-multimeter = r.Readout()
+multimeter = Readout.Readout()
 
 # Read for the duration set
-read = multimeter.readLoop(duration)
+read = multimeter.read_loop(duration)
 
-#============
+# ============
 # Write Data
-#============
+# ============
 
 title = path + date + extension
 
@@ -51,7 +61,6 @@ title = path + date + extension
 # The header has 8 lines all satrting with #
 header = "{0}\nDuration: {7}\nPointing Position of the Horn: {1}\nAngle pointing: {2}\nCalibrator used: " \
          "{3}\nTemperature Outside: {4}\nTemperature of the calibrator: {5}\nWeather: {6}".format(
-    title, looking, angle, calibrator, temperatureOutside,temperatureCalibrator,weather,duration)
+            title, looking, angle, calibrator, temperatureOutside, temperatureCalibrator, weather, duration)
 
-np.savetxt(title,read,header=header)
-
+np.savetxt(title, read, header=header)
